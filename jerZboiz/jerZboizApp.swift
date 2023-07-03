12f6b08c18d1ff71f473419b05e6b42653c13749
Schreck Code <schreckcode@gmail.com>
2023-07-03 13:17:12 -0500

    Initial Commit

---
+++ jerZboiz/jerZboizApp.swift
@@ -0,0 +1,17 @@
+//
+//  jerZboizApp.swift
+//  jerZboiz
+//
+//  Created by Patrick  Schreck on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct jerZboizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
